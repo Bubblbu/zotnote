fd1c11072b380358e4c19680f9e525e07526b73c
bubblbu <asura.enkhbayar@gmail.com>
2020-03-27 01:44:43 -0700

    correct entry point for cli

--- zotnote/__main__.py
+++ zotnote/__main__.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 # # -*- coding: utf-8 -*-
-from zotnote.cli import cli, config, new, report
+from zotnote.cli import cli, config, new, update_templates
 from .config import Configuration
 
 
-if __name__ == "__main__":
+def main():
     Configuration().check_files()
 
     cli.add_command(new)
-    cli.add_command(report)
+    # cli.add_command(report)
     cli.add_command(config)
+    cli.add_command(update_templates)
     cli()
+
+
+if __name__ == "__main__":
+    main()
