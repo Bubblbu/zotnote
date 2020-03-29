f701f572561183ad41fa743f3ea9333d4499324f
bubblbu <asura.enkhbayar@gmail.com>
2020-03-29 00:08:52 -0700

    improve basic note interactions

--- zotnote/__main__.py
+++ zotnote/__main__.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # # -*- coding: utf-8 -*-
-from zotnote.cli import cli, config, new, update_templates, report, edit, remove
+from zotnote.cli import cli
+from zotnote.cli import config, add, update_templates, report, edit, remove
 from .config import Configuration
 
 
 def main():
     Configuration().check_files()
 
-    cli.add_command(new)
+    cli.add_command(add)
     cli.add_command(report)
     cli.add_command(config)
     cli.add_command(update_templates)
--- zotnote/cli.py
+++ zotnote/cli.py
@@ -1,46 +1,32 @@
-# # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import os
 import sys
 from pathlib import Path
 
 import click
 
-from zotnote.connectors.bbp import BetterBibtex
+from zotnote.connectors.bbt import (BetterBibtex, BetterBibtexException,
+                                    BetterBibtexNotRunning)
 
 from .config import Configuration
 from .templates import MarkdownNote
+from .utils import citekey_regex
 
 
-def create_note(citekey):
+def create_note(citekey, config, bbt, force):
     """Create reading note for CITEKEY in your Zotero library."""
-    config = Configuration().load_config()
-
-    candidates = BetterBibtex.search_citekey_in_bbp(citekey)
+    candidates = bbt.search_citekey_in_bbp(citekey)
     if not candidates:
         click.echo("No results found for " + citekey)
         sys.exit()
-    else:
-        if len(candidates) != 1:
-            click.echo("More potential matching articles. More options to be implemented soon.")
-            sys.exit()
-        else:
-            candidate = candidates[0]
-
-    fieldValues = BetterBibtex.extract_fields(candidate)
-
-    click.echo(f"Found entry for {citekey}:\n\n"
-               f"\tTitle: {fieldValues['title']}\n"
-               f"\tCreator: {fieldValues['author']}\n"
-               f"\tDate: {fieldValues['issued']}\n")
-
-    choice = click.confirm("Is this the correct document?")
-    if choice:
-        pass
-    else:
-        click.echo("Cya!")
+    elif len(candidates) != 1:
+        click.echo("Something wrong happened here. We have too many candidates...")
         sys.exit()
+    else:
+        candidate = candidates[0]
+        fieldValues = bbt.extract_fields(candidate)
 
-    # Fill template
+        # Fill template
     md = MarkdownNote(citekey, fieldValues, config)
 
     # Write output file
@@ -48,27 +34,65 @@ def create_note(citekey):
     outfile = notes_dir / f"{citekey}.md"
 
     if outfile.exists():
-        choice = click.confirm('This file already exists. Edit instead?')
-        if choice:
+        if force:
             click.echo(f"Overwriting {str(outfile)}")
             outfile.write_text(str(md))
         else:
-            click.echo("I have not created a new reading note.")
+            choice = click.confirm(
+                'This file already exists. Edit instead?" Use --force to overwrite files.')
+            if choice:
+                os.system(f"{config['editor']} {str(outfile)}")
     else:
         click.echo(f"Writing {str(outfile)}")
         outfile.write_text(str(md))
 
 
-@click.command(help="Create a new note")
-@click.argument('citekey')
-def new(citekey):
-    create_note(citekey)
+@click.command(help="Create a new note. If no citekey is provided the Zotero picker is launched.")
+@click.argument('citekey', required=False)
+@click.option("-f", "--force", is_flag=True, help="Overwrite existing notes")
+def add(citekey, force):
+    config = Configuration.load_config()
+
+    try:
+        bbt = BetterBibtex(config)
+    except BetterBibtexNotRunning as e:
+        click.echo(e)
+        sys.exit()
+
+    if citekey:
+        match = citekey_regex.match(citekey)
+        if match is None:
+            click.echo("The citekey provided is not valid")
+            sys.exit()
+    else:
+        citekey = bbt.citation_picker()
+        if citekey is None:
+            click.echo("No citation key provided.")
+            sys.exit()
+
+    create_note(citekey, config, bbt, force)
 
 
 @click.command(help="Open note in your editor")
 @click.argument('citekey')
 def edit(citekey):
-    config = Configuration().load_config()
+    config = Configuration.load_config()
+
+    try:
+        bbt = BetterBibtex(config)
+    except BetterBibtexNotRunning as e:
+        click.echo(e)
+        sys.exit()
+
+    if citekey:
+        match = citekey_regex.match(citekey)
+        if match is None:
+            click.echo("The citekey provided is not valid")
+            sys.exit()
+    else:
+        citekey = bbt.citation_picker()
+        if citekey is None:
+            sys.exit()
 
     # Write output file
     notes_dir = Path(config['notes'])
@@ -79,7 +103,7 @@ def edit(citekey):
     else:
         choice = click.confirm("File does not exist yet. Create now?")
         if choice:
-            create_note(citekey)
+            create_note(citekey, config)
         else:
             sys.exit()
 
@@ -87,7 +111,23 @@ def edit(citekey):
 @click.command(help="Remove a note")
 @click.argument('citekey')
 def remove(citekey):
-    config = Configuration().load_config()
+    config = Configuration.load_config()
+
+    try:
+        bbt = BetterBibtex(config)
+    except BetterBibtexNotRunning as e:
+        click.echo(e)
+        sys.exit()
+
+    if citekey:
+        match = citekey_regex.match(citekey)
+        if match is None:
+            click.echo("The citekey provided is not valid")
+            sys.exit()
+    else:
+        citekey = bbt.citation_picker()
+        if citekey is None:
+            sys.exit()
 
     # Write output file
     notes_dir = Path(config['notes'])
