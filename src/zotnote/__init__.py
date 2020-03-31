6e277167b4854165f8464d4d7ae66ad90599747e
bubblbu <asura.enkhbayar@gmail.com>
2020-03-31 14:49:25 -0700

    fix missing config file bug

--- src/zotnote/__init__.py
+++ src/zotnote/__init__.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 from xdg import XDG_CONFIG_HOME, XDG_DATA_HOME
 
-project_dir = Path(__file__).parents[1]
+project_dir = Path(__file__).parents[2]
 config_dir = Path(XDG_CONFIG_HOME) / "zotnote"
 data_dir = Path(XDG_DATA_HOME) / "zotnote"
